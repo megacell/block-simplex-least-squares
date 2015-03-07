ff827e123d9656452011c0c8b9ae9804a42d9789
Jerome <jerome.thai.87@gmail.com>
2015-03-06 23:50:13 -0800

    implement multi-block isotonic regression + tests

--- python/isotonic_regression/isotonic_regression_c.pyx
+++ python/isotonic_regression/isotonic_regression_c.pyx
@@ -6,7 +6,7 @@ Compile with python setup.py build_ext --inplace
 
 @author: jerome thai
 
-taken from:
+adapted from:
 http://tullo.ch/articles/speeding-up-isotonic-regression/
 '''
 
@@ -14,24 +14,22 @@ cimport numpy as np
 import numpy as np
 
 def isotonic_regression_c(np.ndarray[np.double_t,ndim=1] y,
-                         np.ndarray[np.double_t,ndim=1] weight):
+                         np.ndarray[np.double_t,ndim=1] weight, start, end):
     cdef:
         np.double_t numerator, denominator
         Py_ssize_t i, pooled, n, k
- 
+    
     n = y.shape[0]
- 
-    if n <= 1:
-        return y
- 
-    n -= 1
+    assert start>=0 and start<n and end>0 and end<=n
+    if start >= end: return y
     while 1:
         # repeat until there are no more adjacent violators.
-        i = 0
+        i = start
         pooled = 0
-        while i < n:
+        end -= 1
+        while i < end:
             k = i
-            while k < n and y[k] >= y[k + 1]:
+            while k < end and y[k] >= y[k + 1]:
                 k += 1
             if y[i] != y[k]:
                 # y[i:k + 1] is a decreasing subsequence, so
@@ -47,7 +45,55 @@ def isotonic_regression_c(np.ndarray[np.double_t,ndim=1] y,
                 pooled = 1
             i = k + 1
         # Check for convergence
-        if pooled == 0:
-            break
+        if pooled == 0: break
  
     return y
+
+
+def isotonic_regression_multi_c(np.ndarray[np.double_t,ndim=1] y,
+                               np.ndarray[np.double_t,ndim=1] weight, blocks):
+    n = y.shape[0]
+    num_blocks = blocks.shape[0]
+    assert False not in ((blocks[1:]-blocks[:-1])>0), 'block indices not increasing'
+    assert blocks[0]>=0 and blocks[-1]<n, 'indices out of range'
+    cdef:
+        np.double_t numerator, denominator
+        Py_ssize_t i, j, pooled, k, start, end
+    j = 0
+    while j < num_blocks:
+        start = blocks[j]
+        end = n
+        if j < num_blocks-1: end = blocks[j+1]
+        
+        while 1:
+            # repeat until there are no more adjacent violators.
+            i = start
+            pooled = 0
+            end -= 1
+            while i < end:
+                k = i
+                while k < end and y[k] >= y[k + 1]:
+                    k += 1
+                if y[i] != y[k]:
+                    # y[i:k + 1] is a decreasing subsequence, so
+                    # replace each point in the subsequence with the
+                    # weighted average of the subsequence.
+                    numerator = 0.0
+                    denominator = 0.0
+                    for j in range(i, k + 1):
+                        numerator += y[j] * weight[j]
+                        denominator += weight[j]
+                    for j in range(i, k + 1):
+                        y[j] = numerator / denominator
+                    pooled = 1
+                i = k + 1
+            # Check for convergence
+            if pooled == 0: break
+
+        j += 1
+
+
+
+
+
+
--- tests/slow/test_stress_isotonic_regression.py
+++ tests/slow/test_stress_isotonic_regression.py
@@ -6,7 +6,7 @@ __author__ = 'jeromethai'
 import sys
 sys.path.append('../../python/isotonic_regression/')
 from isotonic_regression import proj_PAV
-from isotonic_regression_c import isotonic_regression_c
+from isotonic_regression_c import isotonic_regression_c, isotonic_regression_multi_c
 import numpy as np
 from sklearn.isotonic import IsotonicRegression
 from sklearn.utils import check_random_state
@@ -21,7 +21,7 @@ class TestStressIsotonicRegression(unittest.TestCase):
 
     def test_isotonic_regression(self):
         times = []
-        for n in [int(1e3), int(1e4), int(1e5)]:
+        for n in [int(1e3), int(1e4)]:
             x = np.arange(n)
             rs = check_random_state(0)
             y = rs.randint(-50, 50, size=(n,)) + 50. * np.log(1 + np.arange(n))
@@ -35,7 +35,7 @@ class TestStressIsotonicRegression(unittest.TestCase):
 
     def test_proj_PAV(self):
         times = []
-        for n in [int(1e3), int(1e4), int(1e5)]:
+        for n in [int(1e3), int(1e4)]:
             rs = check_random_state(0)
             y = rs.randint(-50, 50, size=(n,)) + 50. * np.log(1 + np.arange(n))
             start_time = time.time()
@@ -47,15 +47,31 @@ class TestStressIsotonicRegression(unittest.TestCase):
 
     def test_isotonic_regression_c(self):
         times = []
-        for n in [int(1e3), int(1e4), int(1e5)]:
+        for n in [int(1e3), int(1e4), int(1e5), int(1e6)]:
             rs = check_random_state(0)
             y = rs.randint(-50, 50, size=(n,)) + 50. * np.log(1 + np.arange(n))
             w = np.ones(n)
             start_time = time.time()
-            isotonic_regression_c(y, w)
+            isotonic_regression_c(y, w, 0, n)
             times.append(time.time() - start_time)
         print 'test isotonic_regression_c'
         print times
+
+
+    def test_isotonic_regression_multi_c(self):
+        n = int(1e6)
+        w = np.ones(n)
+        times = []
+        for num_blocks in [int(1e1), int(1e2), int(1e3), int(1e4)]:
+            rs = check_random_state(0)
+            y = rs.randint(-50, 50, size=(n,)) + 50. * np.log(1 + np.arange(n))
+            blocks = np.sort(np.random.choice(n, num_blocks, replace=False))
+            start_time = time.time()
+            isotonic_regression_multi_c(y, w, blocks)
+            times.append(time.time() - start_time)
+        print 'test isotonic_regression_multi_c'
+        print times
+
 
 
 

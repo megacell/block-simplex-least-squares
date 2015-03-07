a5f12b457066f5f8fb0f3a7e071f551dfb6d8187
Cathy Wu <cathywu@alum.mit.edu>
2015-03-06 23:52:27 -0800

    build direct C implmentation of PAV projection as well (adding this back)

--- python/isotonic_regression/setup.py
+++ python/isotonic_regression/setup.py
@@ -16,11 +16,15 @@ from Cython.Distutils import build_ext
 sourcefiles = ["isotonic_regression_c.pyx"]
 name = "isotonic_regression_c"
 
+simplex_projection = Extension("simplex_projection", 
+        sources = ["simplex_projection.c"],
+        include_dirs=[numpy.get_include()])
+
 setup(
     cmdclass = {"build_ext" : build_ext},
     ext_modules = [Extension(name,
             sourcefiles,
             include_dirs = [numpy.get_include()],
                         language = 'c++',
-            )]
+            ), simplex_projection]
 )

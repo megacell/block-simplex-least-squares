27d8bdc3f87cbbc1809f834af759ca26c0825db6
Jerome <jerome.thai.87@gmail.com>
2015-03-22 15:21:40 -0700

    isotonic fix

--- python/c_extensions/isotonic_regression.h
+++ python/c_extensions/isotonic_regression.h
@@ -49,11 +49,11 @@ void isotonic_regression(double *y, int start, int end) {
         // Check for convergence
         if (pooled == 0) break;
     }
-    i = start
+    i = start;
     while (i < end) {
         k = i + weight[i-start];
         for (j = i + 1; j < k; j++) y[j] = y[i];
-        i += k
+        i += weight[i-start];
     }
 
 }
